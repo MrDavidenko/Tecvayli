9a0b015858703e6c21b1d2233ec6482755f9f2cb
MrDavidenko <145169851+MrDavidenko@users.noreply.github.com>
2025-10-04 23:24:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,35 @@
-const CACHE='tecvayli-monitor-v23';
+const CACHE='tecvayli-monitor-v24';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate',event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).
+      then(()=>self.clients.claim())
   );
 });
 
-// App Shell routing: for navigations, always serve index.html (works around GH Pages 404 in PWA)
+// App Shell routing: for navigations, always serve index.html (avoid GH Pages 404 in PWA)
 self.addEventListener('fetch',event=>{
   const req = event.request;
   if (req.mode === 'navigate') {
     event.respondWith(
-      caches.match('./index.html').then(resp=>{
-        return resp || fetch('./index.html');
-      })
+      caches.match('./index.html').then(resp=>resp || fetch('./index.html'))
     );
     return;
   }
   event.respondWith(
     caches.match(req).then(res=>{
       return res || fetch(req).then(networkRes=>{
-        const copy = networkRes.clone();
+        const copy=networkRes.clone();
         caches.open(CACHE).then(c=>c.put(req, copy));
         return networkRes;
-      });
+      }).catch(()=>caches.match('./index.html'));
     })
   );
 });
