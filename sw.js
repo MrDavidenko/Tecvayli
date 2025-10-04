58331dddff86d411b9119912321b372e9292ceba
MrDavidenko <145169851+MrDavidenko@users.noreply.github.com>
2025-10-04 23:48:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tecvayli-monitor-v27';
+const CACHE='tecvayli-monitor-v28';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
